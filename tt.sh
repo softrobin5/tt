18420c5d80d1232b0f5321835294d185f609704b
softrobin5 <softrobin@gmail.com>
2016-03-03 17:51:52 +0530

    Create tt.sh

---
+++ tt.sh
@@ -0,0 +1,27 @@
+MAXCONN=1000
+HACONFIGSERVERS=/tmp/HACONFIGSERVERS
+HACONFIG=/etc/haproxy/haproxy.cfg
+TMPHACONFIG=/tmp/testconfig
+
+echo "" > $HACONFIGSERVERS
+
+max=`nproc`
+for i in `seq $max`
+do
+    echo "Docker start No. $i"
+        docker run -d -p 808$i:3000 software /bin/bash /var/www/app/start.sh
+	echo  "     server   Tuktuk$i localhost:808$i maxconn $MAXCONN weight 10 check" >> $HACONFIGSERVERS
+	echo "" >> $HACONFIGSERVERS
+done
+
+
+if [ `cat $HACONFIGSERVERS | wc -l` -gt 0 ]; 
+then
+       echo "Writing HA Proxy Config File..."
+       SUBSERVER=`cat $HACONFIGSERVERS | sort -u | sed '/^$/d' | sed -e 's/[\/&]/\\&/g'`
+       cat $HACONFIG | sed -e "/#SERVERBEGIN/,/#SERVEREND/c\\\t\#SERVERBEGIN\n\tSERVERPLACEHOLDER\n\t\#SERVEREND" | perl -p -e "s/SERVERPLACEHOLDER/${SUBSERVER}/g" > $TMPHACONFIG
+       cat $TMPHACONFIG > $HACONFIG
+
+       echo "Reloading HAProxy..."
+#       /etc/init.d/haproxy reload
+fi
